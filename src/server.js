89eb0159baf10d53fdbd0e0b261d419ebee6d354
Guilherme Illescas <GuiIllescas@Guilhermes-MacBook-Pro.local>
2020-06-05 20:20:25 -0300

    Implantação da template engine

--- src/server.js
+++ src/server.js
@@ -3,8 +3,22 @@ const server = express()
 
 server.use(express.static("public"))
 
+const nunjucks = require('nunjucks')
+nunjucks.configure('src/views', {
+  express: server,
+  noCache: true
+})
+
 server.get("/", (req, res) => {
-  res.sendFile(__dirname + '/views/index.html')
+  return res.render('index.njk', { title: 'Opa'})
+})
+
+server.get("/create-point", (req, res) => {
+  return res.render('create-point.njk')
+})
+
+server.get("/search", (req, res) => {
+  return res.render('search-results.njk')
 })
 
 server.listen(3000)
